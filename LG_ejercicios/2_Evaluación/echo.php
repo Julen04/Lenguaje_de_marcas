51097564c962fd226a33153efeb31784579ed12c
AS3-1-17 <AS3-1-17@uni.lan>
2019-12-11 09:16:19 +0100

    d

--- LG_ejercicios/2_Evaluación/echo.php
+++ LG_ejercicios/2_Evaluación/echo.php
@@ -1,6 +1,25 @@
 <?php
 echo "<h1>";
+if (isset($_GET["Nombre"]))
 echo $_GET["Nombre"];
 echo "</h1>";
 echo "<br>";
+if (isset($_GET["Contraseña"]))
 echo $_GET["Contraseña"];
+echo "<br>";
+
+if (isset($_GET["gender"]))
+echo $_GET["gender"];
+
+if (isset($_GET["Opcion1"]))
+echo $_GET["Opcion1"];
+
+if (isset($_GET["Opcion2"]))
+echo $_GET["Opcion2"];
+
+if (isset($_GET["Opcion3"]))
+echo $_GET["Opcion3"];
+
+if (isset($_GET["comment"]))
+echo $_GET["comment"];
+
